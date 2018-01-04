1424459c894d7967ba790e44d1fec64d9b7b402b
raquelasdf <31568431+raquelasdf@users.noreply.github.com>
2018-01-04 15:56:57 -0500

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,35 +3,6 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-var slideIndex1 = 1;
-showSlides1(slideIndex1);
-
-// Next/previous controls
-function plusSlides1(n) {
-  showSlides1(slideIndex1 += n);
-}
-
-// Thumbnail image controls
-function currentSlide1(n) {
-  showSlides1(slideIndex1 = n);
-}
-
-function showSlides1(n) {
-  var i;
-  var slides1 = document.getElementsByClassName("mySlides1");
-  var dots1 = document.getElementsByClassName("dot1");
-  if (n > slides1.length) {slideIndex1 = 1}
-  if (n < 1) {slideIndex1 = slides1.length}
-  for (i = 0; i < slides1.length; i++) {
-      slides1[i].style.display = "none";
-  }
-  for (i = 0; i < dots1.length; i++) {
-      dots1[i].className = dots1[i].className.replace(" active", "");
-  }
-  slides1[slideIndex1-1].style.display = "block";
-  dots1[slideIndex1-1].className += " active";
-}
-
 var slideIndex = 1;
 showSlides(slideIndex);
 
